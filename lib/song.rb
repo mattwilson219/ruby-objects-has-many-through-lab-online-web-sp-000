a7ef33a41c235b6d24a9226c0631ab8c8bee95e5
Matthew Wilson <wilsonmatt219@gmail.com>
2019-05-16 18:17:56 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,13 @@
+class Song
+
+  attr_accessor :name, :artist, :genre
+  @@all = []
+
+  def initialize(name, artist, genre)
+    @name = name
+    @genre = genre
+    @artist = artist
+
+  end
+
+end
