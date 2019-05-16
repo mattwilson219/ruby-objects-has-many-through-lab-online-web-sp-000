8f5af35904dd09e33739135c42f439c302aafb1f
Matthew Wilson <wilsonmatt219@gmail.com>
2019-05-16 18:27:05 +0000

    Automatically backed up by Learn

---
+++ lib/doctor.rb
@@ -0,0 +1,27 @@
+class doctor
+
+  attr_accessor :name
+  @all = []
+
+  def initialize(name)
+    @name = name 
+    @@all << self
+  end
+
+  def appointments
+    Appointment.all.select {|appointment| appointment.doctor == self}
+  end 
+
+  def patients
+    appointments.collect {|appointment| appointment.patient}
+  end
+
+  def self.all 
+    @@all 
+  end
+
+  def new_appointment(patient, date)
+    Appointment.new(patient, date, self)
+  end
+
+end
