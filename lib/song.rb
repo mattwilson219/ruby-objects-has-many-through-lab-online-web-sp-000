59ae0247a727935a6ecaf99a314bd0523b96b7b7
Matthew Wilson <wilsonmatt219@gmail.com>
2019-05-16 18:30:57 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,11 +7,11 @@ class Song
     @name = name
     @genre = genre
     @artist = artist
-    @@all << self 
+    @@all << self
   end
 
-  def self.all 
-    @@all 
+  def self.all
+    @@all
   end
 
 end
